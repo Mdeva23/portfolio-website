70f5c30b9407e5a7f1a99d24b233a58226e30f21
David <davidsedumedi23@gmail.com>
2026-03-22 18:41:44 +0200

    Fixed certificates page

--- obj/Debug/net8.0/PortfolioApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortfolioApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57596c806ea13efe345b5987ecefedaa7ab81aa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2733727a70a6d66f812eeaadb5e8999ab5d0e4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
