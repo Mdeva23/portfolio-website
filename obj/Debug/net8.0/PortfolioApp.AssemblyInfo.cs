f724c30c582bd22beb6ba7b5e7243c82d865f123
David <davidsedumedi23@gmail.com>
2026-03-22 21:11:03 +0200

    Download cv was not working Fixed that part

--- obj/Debug/net8.0/PortfolioApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortfolioApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc4a4c9f5d05d2b9f2dc1c6be2d17592c5bf9f3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db82745b6bdeb56a6e9674158d4ce5d8368a3afa")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
